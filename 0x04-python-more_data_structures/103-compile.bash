d0f6941e40fec3874150bddacec968068991448a
Aishatibe <suleaishat990@gmail.com>
2022-06-16 06:26:43 -0700

    more data structures

---
+++ 0x04-python-more_data_structures/103-compile.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+# PY_BIN_PATH=/usr/bin/python3.8
+# PY_INC_PATH=/usr/include/python3.8
+PY_BIN_PATH=/usr/bin/python3.4
+PY_INC_PATH=/usr/include/python3.4
+# PY_BIN_PATH=~/apps/Python-3.4.10/python
+# PY_INC_PATH=~/apps/Python-3.4.10/Include/
